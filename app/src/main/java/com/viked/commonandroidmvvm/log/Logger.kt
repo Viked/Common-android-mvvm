0146c7b53b8748965ee322ecd12a7e676b7286e3
Yevgenii Shein <initbin@gmail.com>
2018-02-28 00:05:16 +0200

    Fixed migration

--- app/src/main/java/com/viked/commonandroidmvvm/log/Logger.kt
+++ app/src/main/java/com/viked/commonandroidmvvm/log/Logger.kt
@@ -1,17 +1,18 @@
 package com.viked.commonandroidmvvm.log
 
 import android.util.Log
-import com.crashlytics.android.Crashlytics
-import com.viked.commonandroidmvvm.BuildConfig
 
 /**
  * Created by yevgeniishein on 10/9/17.
  */
 
 fun Throwable.log() {
+    Log.e("Error", message ?: "", this)
+    /*
     if (BuildConfig.DEBUG) {
         Log.e("Error", message ?: "", this)
     } else {
         Crashlytics.logException(this)
     }
+    */
 }
