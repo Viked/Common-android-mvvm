ebf6550685f378a9291fb750df0cc69e118f5f97
Yevgenii Shein <initbin@gmail.com>
2018-09-06 18:20:16 +0300

    Edited dagger

--- app/src/main/java/com/viked/commonandroidmvvm/di/BaseAppModule.kt
+++ app/src/main/java/com/viked/commonandroidmvvm/di/BaseAppModule.kt
@@ -1,14 +1,17 @@
 package com.viked.commonandroidmvvm.di
 
+import android.arch.lifecycle.ViewModelProvider
 import com.evernote.android.job.JobCreator
 import com.viked.commonandroidmvvm.bg.JobHelper
 import dagger.Binds
 import dagger.Module
 
 @Module
-abstract class JobCreatorModule {
+abstract class BaseAppModule {
 
     @Binds
     abstract fun bindJobHelper(jobHelper: JobHelper): JobCreator
 
+    @Binds
+    abstract fun bindViewModelFactory(factory: ViewModelFactory): ViewModelProvider.Factory
 }
