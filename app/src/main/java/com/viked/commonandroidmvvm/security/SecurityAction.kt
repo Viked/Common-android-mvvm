58b9d5309af26e62d5e264f5fba21c4c235a5113
Yevhenii Shein <initbin@gmail.com>
2022-07-31 22:30:51 +0300

    Update permission logic

--- app/src/main/java/com/viked/commonandroidmvvm/security/SecurityAction.kt
+++ app/src/main/java/com/viked/commonandroidmvvm/security/SecurityAction.kt
@@ -2,14 +2,21 @@ package com.viked.commonandroidmvvm.security
 
 import android.content.Context
 import android.content.pm.PackageManager
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContract
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 
 abstract class Security {
-    abstract fun getContract(): ActivityResultContract<*, *>
-    abstract fun requestPermission(context: Context, launcher: ActivityResultLauncher<*>)
+
+    abstract fun initWithRegistry(appCompatActivity: AppCompatActivity, callback: Runnable)
+    abstract fun initWithRegistry(fragment: Fragment, callback: Runnable)
+    abstract fun requestPermission(context: Context)
     abstract fun needPermissionRequest(context: Context): Boolean
+
+    fun executeCallback(callback: Runnable) =
+        Handler(Looper.getMainLooper()).postDelayed({ callback.run() }, 300)
 
     fun checkAndroidPermission(context: Context, vararg permissions: String): Boolean =
         permissions.any {
